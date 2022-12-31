70e7afbb67aed86392bc0b371f56f41a3219734a
jujumilk3 <jujumilk3@gmail.com>
2022-12-31 19:58:40 +0900

    test

---
+++ test_auto_fix.py
@@ -0,0 +1,10 @@
+import os
+test = "Hi"                            
+
+
+def test():
+    print("Hello World")
+    a = 1
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'testproject.settings')
