48f8e1f914fcce71d32f8697aaf8d9e763934bd9
jujumilk3 <jujumilk3@gmail.com>
2025-09-12 15:59:24 +0900

    wip: sample mcp server

---
+++ mcp-server-test/README.md
@@ -0,0 +1,6 @@
+# mcp-server-test
+
+## description
+
+MCP server is stateful, but modern deployment environments like EKS, GKE, AKS are stateless.
+This project was created to test MCP server in a stateless manner
---
+++ mcp-server-test/sample-mcp-server/main.py
@@ -0,0 +1,25 @@
+import os
+
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
+
+
+if __name__ == "__main__":
+    # MCP_SERVER_MODE: stdio | streamable-http | sse
+    MCP_SERVER_MODE = os.environ.get("MCP_SERVER_MODE", "stdio").lower()
